b4a4361f4e09a03eeeda283bbb58611941832a94
Henrik Stridsman <h.stridsman@gmail.com>
2015-05-19 16:32:50 +0200

    Update main.c

--- MFLcremedelacreme/trunk/MFLcremedelacreme/main.c
+++ MFLcremedelacreme/trunk/MFLcremedelacreme/main.c
@@ -1,5 +1,42 @@
-#include <stdio.h>
+#include "scanadt.h"
+#include "genlib.h"
+#include "exception.h"
 
 main(){
+	scannerADT scanner;
+	string line, token;
 
+	scanner = NewScanner();
+	GetScannerSpaceOption(scanner, IgnoreSpaces);
+
+	while (TRUE){
+		try
+		line = GetLine();
+		SetScannerString(scanner, line);
+		token = ReadToken(scanner);
+
+		if (token == ':'){
+			token = ReadToken(scanner);
+			if (token == 'l' || token == "load"){
+
+			}
+			else if (token == 'd' || token == "define"){
+
+			}
+			else if(token == 'h' || token == "help"){
+
+			}
+			else if (token == 'q' || token == "quit"){
+				exit(0);
+			}
+			else{
+				Error("Invalid input");
+			}
+
+		}
+		else{
+
+		}
+
+	} endtry
 }
