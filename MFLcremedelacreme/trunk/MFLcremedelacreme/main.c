488ef9c46f16c03748722b75ab782dd9403eaa86
Henrik Stridsman <h.stridsman@gmail.com>
2015-05-25 15:16:06 +0200

    Update main.c

--- MFLcremedelacreme/trunk/MFLcremedelacreme/main.c
+++ MFLcremedelacreme/trunk/MFLcremedelacreme/main.c
@@ -1,105 +1,244 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include "scanadt.h"
+#include "parser.h"
+#include <stdio.h>
+#include <ctype.h>
 #include "genlib.h"
 #include "strlib.h"
-#include "exception.h"
-#include "env.h"
-#include "symtab.h"
-#include "parser.h"
-#include "eval.h"
+#include "simpio.h"
+#include "exp.h"
+#include "scanadt.h"
 
 
-void loadFunction(scannerADT scanner, symtabADT table){
-	FILE * infile;
-	string fileName, key = NULL;
-	int value = 0;
 
-	fileName = GetLine();
-	infile = fopen(fileName, "r");
+/*
+* Type: expADT
+* -------------------
+* This type is used to represent the abstract notion of any
+* expression in MFL.
+*
+*   E -> T + E
+*   E -> T - E
+*   E -> T
+*
+*   T -> C * T
+*   T -> C / T
+*   T -> C
+*
+*   C -> F (E)
+*   C -> F
+*
+*   F -> (E)
+*   F -> if E RelOp E then E else E
+*   F -> func (identifier) { E }
+*   F -> integer
+*   F -> identifier
+*
+*   RelOp -> =
+*   RelOp -> <
+*   RelOp -> >
+*
+*/
+static expADT ReadE(scannerADT scanner);
+static expADT ReadT(scannerADT scanner);
+static expADT ReadC(scannerADT scanner);
+static expADT ReadF(scannerADT scanner);
 
-	if (infile == NULL){
-		Error("Can't open file.");
+/*
+* Implementation notes: ParseExp
+* ------------------------------
+* This function just calls ReadE to read an expression and then
+* checks to make sure no tokens are left over.
+*/
+
+expADT ParseExp(scannerADT scanner)
+{
+	expADT exp;
+
+	exp = ReadE(scanner);
+	if (MoreTokensExist(scanner)) {
+		Error("ParseExp: %s unexpected", ReadToken(scanner));
+	}
+	return (exp);
+}
+
+/* Function: readE
+*
+*
+*   E -> T + E
+*   E -> T - E
+*   E -> T
+*/
+
+static expADT ReadE(scannerADT scanner)
+{
+	expADT exp, rhs;
+	string token;
+	int newPrec;
+
+	exp = ReadT(scanner);
+	token = ReadToken(scanner);
+	if (IsPlusMinusOperator(token)){
+		rhs = ReadE(scanner);
+		exp = NewCompoundExp(token[0], exp, rhs);
+	}
+	else{
+		SaveToken(scanner, token);
+	}
+
+
+	return (exp);
+}
+
+/*   Function: readT
+*
+*   T -> C * T
+*   T -> C / T
+*   T -> C
+*/
+
+static expADT ReadT(scannerADT scanner){
+	expADT exp, rhs;
+	string token;
+	int newPrec;
+
+	exp = ReadC(scanner);
+	token = ReadToken(scanner);
+	if (IsTimesDivOperator(token)){
+		rhs = ReadT(scanner);
+		exp = NewCompoundExp(token[0], exp, rhs);
+	}
+	else{
+		SaveToken(scanner, token);
+	}
+	return (exp);
+}
+
+/*   Function: readC
+*
+*   C -> F (E)
+*   C -> F
+*/
+
+static expADT ReadC(scannerADT scanner){
+	expADT exp, rhs;
+	string token;
+
+	exp = ReadF(scanner);
+	token = ReadToken(scanner);
+	if (StringEqual(token, "(")) {
+		rhs = ReadE(scanner);
+		if (!StringEqual(ReadToken(scanner), ")")){
+			Error("Unbalanced parentheses");
+		}
+		exp = NewCallExp(exp, rhs);		//C -> F (E)
 	}
 	else {
-		/*while (fscanf(infile, "%s %d", key, parserShit()) != EOF) { //key är oinstansierad.
-			Enter(table, key, parserShit());
-		}*/
+		SaveToken(scanner, token);
 	}
-	fclose(fileName);
+	return(exp);
 }
-void defineFunction(scannerADT scanner, symtabADT table){
+
+/*	 Function: readF
+*
+*   F -> (E)
+*   F -> if E RelOp E then E else E
+*   F -> func (identifier) { E }
+*   F -> integer
+*   F -> identifier
+*/
+
+static expADT ReadF(scannerADT scanner){
+	expADT exp = NULL, lhs, rhs = NULL, ifPart, elsePart;
 	string token;
-	int value = 0;
+	char relop;
+
 	token = ReadToken(scanner);
-	//somefunction that does the rest of this shit.
-
-	Enter(table, token, value);
-}
-
-
-void helpFunction(){
-	printf("\n\n");
-
-	printf("My name is A.I - helper, I'm going to write out the commands:\n");
-	printf("1: => Load - To load an existing file.\n");
-	printf("2: => Define - define a variable ex. x = 2 + 1.\n");
-	printf("3: => Help - You'r already here, so you know what this one does.\n");
-	printf("4: => Exit - This will exit the program.");
-}
-
-void quitFunction(){
-	exit(0);
-}
-
-main(){
-	scannerADT scanner;
-	string line, token;
-	environmentADT env;
-	symtabADT table;
-	expADT exp;
-	valueADT value;
-
-	void(*functionPtr)(scannerADT, symtabADT);
-
-	scanner = NewScanner();
-	env = NewEnvironment();
-	table = NewSymbolTable();
-
-	GetScannerSpaceOption(scanner, IgnoreSpaces);
-
-	/*Enter(table, 'l', &loadFunction);
-	Enter(table, "load", &loadFunction);
-	Enter(table, 'd', &defineFunction);
-	Enter(table, "define", &defineFunction);*/
-	Enter(table, "h", &helpFunction);
-	Enter(table, "help", &helpFunction);
-	Enter(table, "q", &quitFunction);
-	Enter(table, "quit", &quitFunction);
-
-
-	while (TRUE)try{
-
-		printf("=> ");
-		line = GetLine();
-		SetScannerString(scanner, line);
+	if (StringEqual(token, "(")) {
+		exp = ReadE(scanner);				//F -> (E)
+		if (!StringEqual(ReadToken(scanner), ")")) {
+			Error("Unbalanced parentheses");
+		}
+	}
+	else if (StringEqual(token, "if"))
+	{
+		lhs = ReadE(scanner);
 		token = ReadToken(scanner);
-		
+		if (IsRealOp(token)){
+			rhs = ReadE(scanner);
+			if (StringEqual(ReadToken(scanner), "then")){
+				ifPart = ReadE(scanner);
+				if (StringEqual(ReadToken(scanner), "else")){
+					elsePart = ReadE(scanner);
+					exp = NewIfExp(lhs, token, rhs, ifPart, elsePart); //F -> if E RelOp E then E else E
+				}
+			}
+		}
 
-		if (StringEqual(token, ":")){
+	}
+	else if (StringEqual(token, "func")){
+		if (StringEqual(token, "(")) {
 			token = ReadToken(scanner);
-			if (Lookup(table, token) != UNDEFINED){
-				functionPtr = Lookup(table, token);
-				(*functionPtr)(scanner, table);
+			if (!StringEqual(ReadToken(scanner), ")")) {
+				Error("Unbalanced parentheses");
 			}
-			else{
-				Error("Invalid input");
+			if (StringEqual(token, "{")) {
+				rhs = ReadE(scanner);
+				if (!StringEqual(ReadToken(scanner), "}")) {
+					Error("Unbalanced parentheses");
+				}
 			}
-
+			exp = NewFuncExp(token, rhs); // F -> func (identifier) { E }
 		}
-		else{
-			exp = ParseExp(scanner);
-			value = Eval(exp, env);
-		}
+	}
+	else if (isalpha(token[0])){
+		exp = NewIdentifierExp(token); // F -> identifier
+	}
+	else if (isdigit(token[0])){
+		exp = NewIntegerExp(StringToInteger(token)); // F -> integer
+	}
+	else {
+		SaveToken(scanner, token);
+	}
+	return(exp);
+}
 
-	} endtry
+
+static bool IsPlusMinusOperator(string token){
+	if (StringLength(token) != 1){
+		return FALSE;
+	}
+	switch (token[0]){
+	case '+':
+	case '-':
+		return TRUE;
+	default:
+		return FALSE;
+	}
+}
+
+
+static bool IsTimesDivOperator(string token){
+	if (StringLength(token) != 1){
+		return FALSE;
+	}
+	switch (token[0]){
+	case '*':
+	case '/':
+		return TRUE;
+	default:
+		return FALSE;
+	}
+}
+
+static bool IsRealOp(string token){
+	if (StringLength(token) != 1){
+		return FALSE;
+	}
+	switch (token[0]){
+	case '=':
+	case '<':
+	case '>':
+		return TRUE;
+	default:
+		return FALSE;
+	}
 }
